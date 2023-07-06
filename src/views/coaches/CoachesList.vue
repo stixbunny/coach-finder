39a55ec422ceca4ebd8804f8d2e92c4a41deb573
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-06 18:02:27 -0400

    list of coaches

--- src/views/coaches/CoachesList.vue
+++ src/views/coaches/CoachesList.vue
@@ -7,12 +7,19 @@
 			<button>Refresh</button>
 			<RouterLink to="/register">Register as Coach</RouterLink>
 		</div>
-		LIST OF COACHES
+		<ul v-if="coachesStore.hasCoaches()">
+			<li v-for="coach in coachesStore.coaches" :key="coach.id">
+				{{ coach.firstName }}
+			</li>
+		</ul>
+		<h3 v-else>No coaches found.</h3>
 	</section>
 </template>
 
 <script setup>
+import { useCoachesStore } from '../../stores/coaches';
 
+const coachesStore = useCoachesStore();
 </script>
 
 <style scoped>
