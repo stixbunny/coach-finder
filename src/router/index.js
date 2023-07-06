8ec96395b56906032667858c32c2781f5e312c2e
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-06 12:59:41 -0400

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/coaches',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/coaches',
+      component: null,
+    },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [
+        {
+          path: '/contact',
+          component: null,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      component: null,
+    },
+    {
+      path: '/requests',
+      component: null,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: null,
     },
   ],
 });
