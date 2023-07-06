fe38e5681ca92fd909611e9ba9ceb37c3d3a92a8
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-06 13:20:59 -0400

    base app style

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@ import { RouterView } from 'vue-router';
   <RouterView />
 </template>
 
-<style scoped>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
