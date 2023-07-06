9e679f795ffdcd57a3d5a22f89002d8a557b1bdc
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-06 13:27:01 -0400

    TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import TheHeader from './components/layout/TheHeader.vue';
 </script>
 
 <template>
+  <TheHeader />
   <RouterView />
 </template>
 
