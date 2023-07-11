39ede777c7cba80408e77edc117164877e71af0c
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-10 20:09:27 -0400

    requests store

--- src/stores/requests.js
+++ src/stores/requests.js
@@ -1,8 +1,18 @@
 // import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
 export const useRequestsStore = defineStore('requests', () => {
-  
+  const requests = ref([]);
 
-  return {  };
+  function addRequest(request) {
+    const newRequest = {
+      id: new Date().toISOString(),
+      coachId: request.coachId,
+      userEmail: request.email,
+      message: request.message,
+    };
+    requests.value.push(newRequest);
+  }
+  return { requests, addRequest };
 });
