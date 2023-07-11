212769c5aedb0afd3ff92e5c9c82265db885158a
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-10 21:35:42 -0400

    RequestItem component

---
+++ src/components/requests/RequestItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <div>
+      <a :href="emailLink">{{ props.email }}</a>
+    </div>
+    <p>{{ props.message }}</p>
+  </li>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(['email', 'message']);
+
+const emailLink = computed(() => 'mailto:' + props.email);
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+a {
+  color: #3d008d;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+a:hover,
+a:active {
+  color: #8d007a;
+}
+
+p {
+  margin: 0.5rem 0 0 0;
+}
+</style>
