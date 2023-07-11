c78100a2e08a615dd74cd4f2ba7ad9c765f55b27
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-11 19:44:24 -0400

    route animation

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@ import TheHeader from './components/layout/TheHeader.vue';
 
 <template>
   <TheHeader />
-  <RouterView />
+  <RouterView v-slot="slotProps">
+    <Transition name="route" mode="out-in">
+      <Component :is="slotProps.Component"></Component>
+    </Transition>
+  </RouterView>
 </template>
 
 <style>
@@ -22,4 +26,24 @@ html {
 body {
   margin: 0;
 }
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
 </style>
