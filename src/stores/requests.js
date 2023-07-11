8eac138bc9425a2a3346e7931852b22f1db57ba1
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-11 17:39:21 -0400

    requests done

--- src/stores/requests.js
+++ src/stores/requests.js
@@ -7,28 +7,56 @@ export const useRequestsStore = defineStore('requests', () => {
   const firebaseUrl = import.meta.env.VITE_API_FIREBASE;
 
   const user = useMainStore();
-  
+
   const requests = ref([]);
 
-  function myRequests() {
-    const userId = user.userId;
-    return requests.value.filter((req) => req.coachId === userId);
-  }
-
-  function addRequest(request) {
+  async function addRequest(request) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: request.coachId,
       userEmail: request.email,
       message: request.message,
     };
+    const response = await fetch(`${firebaseUrl}/requests/${request.coachId}.json`, {
+      method: 'POST',
+      body: JSON.stringify(newRequest),
+    });
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to send request.');
+      throw error;
+    }
+    newRequest.id = responseData.name;
+    newRequest.coachId = request.coachId;
     requests.value.push(newRequest);
   }
 
-  function hasRequests() {
-    const reqs = myRequests();
-    return reqs && reqs.length > 0;
+  async function fetchRequests() {
+    const coachId = user.userId;
+    const response = await fetch(`${firebaseUrl}/requests/${coachId}.json`);
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch requests.');
+      throw error;
+    }
+    const requests = [];
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(request);
+    }
+    setRequests(requests);
   }
 
-  return { myRequests, addRequest, hasRequests };
+  function setRequests(fetchedRequests) {
+    requests.value = fetchedRequests;
+  }
+
+  function hasRequests() {
+    return requests.value && requests.value.length > 0;
+  }
+
+  return { requests, addRequest, hasRequests, fetchRequests };
 });
--- src/views/requests/RequestsReceived.vue
+++ src/views/requests/RequestsReceived.vue
@@ -1,27 +1,52 @@
 <template>
+  <BaseDialog :show="!!error" title="An error ocurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </BaseDialog>
   <section>
     <BaseCard>
       <header>
         <h2>Requests Received</h2>
-        <ul v-if="requestsStore.hasRequests()">
-          <RequestItem
-            v-for="req in requestsStore.myRequests()"
-            :key="req.id"
-            :email="req.userEmail"
-            :message="req.message"
-          ></RequestItem>
-        </ul>
-        <h3 v-else>You haven't received any requests yet!</h3>
       </header>
+      <BaseSpinner v-if="isLoading"></BaseSpinner>
+      <ul v-else-if="requestsStore.hasRequests() && !isLoading">
+        <RequestItem
+          v-for="req in requestsStore.requests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        ></RequestItem>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
     </BaseCard>
   </section>
 </template>
 
 <script setup>
+import { onBeforeMount, ref } from 'vue';
 import RequestItem from '../../components/requests/RequestItem.vue';
 import { useRequestsStore } from '../../stores/requests';
 
 const requestsStore = useRequestsStore();
+const isLoading = ref(false);
+const error = ref(null);
+
+async function loadRequests() {
+  isLoading.value = true;
+  try {
+    await requestsStore.fetchRequests();
+  } catch (e) {
+    error.value = e.message || 'Something failed!';
+  } 
+  isLoading.value = false;
+}
+
+function handleError() {
+  error.value = null;
+}
+
+onBeforeMount(() => {
+  loadRequests();
+});
 </script>
 
 <style scoped>
