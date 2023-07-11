ba5d895211a5e9c976132d7e2192000009224216
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-10 20:01:13 -0400

    basic ContactCoach

--- src/views/requests/ContactCoach.vue
+++ src/views/requests/ContactCoach.vue
@@ -1,3 +1,76 @@
 <template>
-	<h1>Contact Coach</h1>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-Mail</label>
+      <input type="email" id="email" v-model.trim="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea rows="5" id="message" v-model.trim="message"></textarea>
+    </div>
+    <p class="errors" v-if="!formIsValid">Please enter a valid email and non-empty message.</p>
+    <div class="actions">
+      <BaseButton>Send Message</BaseButton>
+    </div>
+  </form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+const message = ref('');
+const formIsValid = ref(true);
+
+function submitForm() {
+  formIsValid.value = true;
+  if (email.value === '' || !email.value.includes('@') || message.value === '') {
+    formIsValid.value = false;
+    return;
+  }
+}
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
