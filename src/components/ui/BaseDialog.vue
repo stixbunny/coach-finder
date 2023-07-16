45520534d204568897613134a2beae306381e1a7
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-16 19:31:02 -0400

    script setup syntax

--- src/components/ui/BaseDialog.vue
+++ src/components/ui/BaseDialog.vue
@@ -21,33 +21,29 @@
   </teleport>
 </template>
 
-<script>
-export default {
-  props: {
-    show: {
-      type: Boolean,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: false,
-    },
-    fixed: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
+<script setup>
+const props = defineProps({
+  show: {
+    type: Boolean,
+    required: true,
   },
-  emits: ['close'],
-  methods: {
-    tryClose() {
-      if (this.fixed) {
-        return;
-      }
-      this.$emit('close');
-    },
+  title: {
+    type: String,
+    required: false,
   },
-};
+  fixed: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+const emit = defineEmits(['close']);
+function tryClose() {
+  if (props.fixed) {
+    return;
+  }
+  emit('close');
+}
 </script>
 
 <style scoped>
